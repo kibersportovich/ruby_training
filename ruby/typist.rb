0c6ddbfe53f49f0b2348cc7434dff0bd6a8ab3c6
kibersportovich <unconfigured@null.spigotmc.org>
2023-09-22 20:10:36 +0300

    Typist

---
+++ ruby/typist.rb
@@ -0,0 +1,66 @@
+:begin
+Task
+
+John is a typist. He has a habit of typing: he never use the Shift key to switch case, just using only Caps Lock.
+
+Given a string s. Your task is to count how many times the keyboard has been tapped by John.
+
+You can assume that, at the beginning the Caps Lock light is not lit.
+
+Input/Output
+[input] string s
+
+A non-empty string. It contains only English letters(uppercase or lowercase).
+
+1 ≤ s.length ≤ 10000
+
+[output] an integer
+
+The number of times John tapped the keyboard.
+
+Example
+For s = "a", the output should be 1.
+
+John hit button a.
+
+For s = "aa", the output should be 2.
+
+John hit button a, a.
+
+For s = "A", the output should be 2.
+
+John hit button Caps Lock, A.
+
+For s = "Aa", the output should be 4.
+
+John hit button Caps Lock, A, Caps Lock, a.
+
+:end
+
+def typist(s)
+  caps = 0
+  up = ('A'..'Z').to_a
+  press = 0
+  s.chars.each do |ch|
+    if up.include? ch
+      if caps == 0
+        press += 1
+        caps = 1
+      end
+      press += 1
+    else
+      if caps == 1
+        press += 1
+        caps = 0
+      end
+      press += 1
+    end
+  end
+  press
+end
+
+
+
+
+
+
